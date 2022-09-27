dfbe757aeaa75fc208a5162695b4da89fc5ee9d8
alexcarrion02 <arcarc2001@gmail.com>
2022-09-27 17:42:41 +0200

    gitignore and request param

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -4,14 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
 	@GetMapping("/string")
-	public String param(Model model) {
-		
+	public String param(@RequestParam(name="texto",required = false,defaultValue = "algun valor...") String texto,Model model) {
+		model.addAttribute("resultado","El texto enviado es: "+texto);
 		return "params/ver";
 		
+	}
+	@GetMapping("/")
+	public String param() {
+		return "params/index";	
 	}
 }
